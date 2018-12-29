d97437353668e5ad5958978d4fe159683cd7a6b3
ZhiZhao-Hong <983130285@qq.com>
2018-12-29 16:19:11 +0800

    删除了flask的项目

--- mooc/Flask_Note/fisher/app/secure.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-# 数据库等比较机密，写到这里。 本文件不上传到git
-DEBUG = True
-
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:heygears@localhost:3306/fisher'
-SECRET_KEY = 'd456as4d3z1c3165d4q8d4a5c1z3c1z53c4a56sd4'
